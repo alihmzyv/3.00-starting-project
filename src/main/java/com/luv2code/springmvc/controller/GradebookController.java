902c5e19db41e0bbfc6ba007ff8d494a9e64e641
alihmzyv <alihmzyv@gmail.com>
2023-01-09 01:21:21 +0400

    used constructor injection. deleted get mapping which is not implemented yet. added post and get (which is weird) for creating and deleting student.

--- src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -1,9 +1,8 @@
 package com.luv2code.springmvc.controller;
 
-import com.luv2code.springmvc.models.*;
+import com.luv2code.springmvc.models.Gradebook;
 import com.luv2code.springmvc.models.student.CollegeStudent;
 import com.luv2code.springmvc.service.StudentAndGradesService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/")
 public class GradebookController {
+	private final Gradebook gradebook;
+	private final StudentAndGradesService studentService;
 
-	@Autowired
-	private Gradebook gradebook;
-
-	@Autowired
-	private StudentAndGradesService studentService;
+	public GradebookController(Gradebook gradebook, StudentAndGradesService studentService) {
+		this.gradebook = gradebook;
+		this.studentService = studentService;
+	}
 
 	@GetMapping("/")
 	public String getStudents(Model m) {
@@ -24,10 +24,17 @@ public class GradebookController {
 		return "index";
 	}
 
-
-	@GetMapping("/studentInformation/{id}")
-	public String studentInformation(@PathVariable Integer id, Model m) {
-		return "studentInformation";
+	@PostMapping("/")
+	public String createStudent(@ModelAttribute("student") CollegeStudent collegeStudent, Model m) {
+		studentService.saveStudent(collegeStudent);
+		m.addAttribute("students", studentService.findAll());
+		return "index";
 	}
 
+	@GetMapping("/delete/student/{id}")
+	public String deleteStudent(@PathVariable Integer id, Model m) {
+		studentService.deleteStudentById(id);
+		m.addAttribute("students", studentService.findAll());
+		return "index";
+	}
 }
